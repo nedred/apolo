72137ffe5165cea0802f85a56a176bcf845a29dc
Angel <angelp.popov@gmail.com>
2020-01-21 16:01:45 +0200

    sync example

--- src/server/urls.py
+++ src/server/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   # path('', accounts.urls),
+    path('', TemplateView.as_view(template_name="index.html"), name='index'),
+    path('login/', TemplateView.as_view(template_name="login.html"), name='index'),
 ]
